3fa59323721282517d427e7583045c28f4ae1593
Doug McGeehan <djmvfb@mst.edu>
2014-04-16 17:43:36 -0500

    Added code to allow for execution

--- UI.py
+++ UI.py
@@ -16,15 +16,22 @@ from cocos.menu import MenuItem
 from cocos.draw import Line
 from cocos.scene import Scene
 import pyglet
+import threading
 
 class UI:
     class Canvas(cocos.draw.Canvas):
         def render(self):
             x, y = director.get_window_size()
 
-            build_road_map_thingie = [ [[100, 400], [100, 100]], [[100, 400], [200, 250]], [[200, 250], [400, 250]],
-                                       [[400, 250], [500, 400]], [[500, 400], [500, 100]], [[500, 100], [400, 250]],
-                                       [[100, 100], [200, 250]] ]
+            build_road_map_thingie = [
+              [[100, 400], [100, 100]],
+              [[100, 400], [200, 250]],
+              [[200, 250], [400, 250]],
+              [[400, 250], [500, 400]], 
+              [[500, 400], [500, 100]], 
+              [[500, 100], [400, 250]],
+              [[100, 100], [200, 250]]
+            ]
 
             for (startCoord, endCoord) in build_road_map_thingie:
                 start = startCoord[0], startCoord[1]
@@ -36,24 +43,30 @@ class UI:
                 self.move_to(start)
                 self.line_to(end)
 
+
     class Layer(cocos.layer.Layer):
         def __init__(self):
             super(UI.Layer, self).__init__()
 
             self.add(UI.Canvas())
             self.schedule(lambda x: 0)
+
+
 class Entity():
-    import threading
     def __init__(self, name, speed, direction):
         self._name = name
         self._speed = speed
         self._direction = direction
 
     def Start(self):
+        pass
 
     def Stop(self):
+        pass
 
     def GetLocation(self):
+        pass
+
 
 class EntityManager():
     def __init__(self, entityNum, speed, directions):
@@ -64,16 +77,17 @@ class EntityManager():
             self._entityList.append(entity)
 
     def Start(self):
+        pass
 
     def Stop(self):
+        pass
 
 
 def main():
     cocos.director.director.init(caption = "Coolest project ever")
-
     scene = Scene(UI.Layer())
-
     cocos.director.director.run(scene)
+
 
 if __name__ == "__main__":
     main()

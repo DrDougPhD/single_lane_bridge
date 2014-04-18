ae1f113294ab095e21b307480154397c4f1e7ff3
madamelic <madeline.a.cameron@gmail.com>
2014-04-17 22:32:43 -0500

    Removed more repeated functions Added beginning of threading of entities Started on entity.start() Trying to think of a way to do it Added speed label and speed text Worked on getting speed updated correctly Added UI.Layer.redraw_speed Added EntityManager.set_layer_obj

--- UI.py
+++ UI.py
@@ -1,3 +1,13 @@
+#UI.py
+#
+#Written by Madeline Cameron and Doug McGeehan
+#CS 384 - Distributed Operating Systems
+#Spring 2014
+#
+#Info:
+#   Speed is Units per Tick
+#   Speed limit is 250 upt
+
 import sys
 import os
 
@@ -49,15 +59,15 @@ class UI:
             for entity in entityList:
                 self.add(entity.get_sprite())
 
+            speedLabel = cocos.text.Label("Speed: ", position=(545, 450),
+                                          color=(200, 200, 200, 200))
+            self._speedText = cocos.text.Label(str(entityManager.get_speed()), position=(600, 450),
+                                               color=(200, 200, 200, 200))
+            self.add(speedLabel)
+            self.add(self._speedText)
 
-class Entity():
-    import threading
-
-    running = True
-    pause = False
-
-    def __init__(self, id, name, speed, direction, pos):
-        self._id = id
+        def redraw_speed(self, speed):
+            self._speedText.element.text = str(speed)
 
 
 class Entity():
@@ -65,10 +75,10 @@ class Entity():
         self._name = name
         self._speed = speed
         self._direction = direction
-        self._pos = pos
         self._status = "Moving"
         self._sprite = cocos.sprite.Sprite('car.png', scale=0.10)
-        self._sprite.position = 100, 100
+        self._sprite.position = pos
+        self._running = False
 
     def get_id(self):
         return self._id
@@ -89,6 +99,9 @@ class Entity():
         return self._sprite
 
     def start(self):
+        #        running = True
+        #        while running:
+        #           self._sprite.do(MoveTo())
         pass
 
     def stop(self):
@@ -101,32 +114,55 @@ class EntityManager():
     def __init__(self, entityNum, speed, directions):
         self._speed = speed
         self._entityList = []
+        self._threadList = []
+        self._layer = None
 
         for i in range(entityNum):
-            entity = Entity(i, "bleh", speed, directions[i], (0, 0))
+            entity = Entity("bleh", speed, (100, 100), directions[i])
+            entityThread = threading.Thread(None, entity.start(), "Entity" + str(i))
             self._entityList.append(entity)
-
-    def start(self):
-        for entity in self._entityList:
-            entity.Start()
-
-    def stop(self):
-        for entity in self._entityList:
-            entity.Stop()
+            self._threadList.append(entityThread)
 
     def get_entity_list(self):
         return self._entityList
 
+    def get_speed(self):
+        return self._speed
+
+    def set_layer_obj(self, layer):
+        self._layer = layer
+
+    def set_speed(self, speed):
+        self._speed = speed
+        self._layer.redraw_speed()
+
+        for entity in self._entityList:
+            entity.set_speed(speed)
+
+    def start(self):
+        for thread in self._threadList:
+            thread.start()
+
+    def stop(self):  #This may not even be needed...
+        for thread in self._threadList:
+            index = self._threadList.index(thread)
+            entityObj = self._entityList[index]
+            entityObj.stop()
+            thread.join()
+
     def on_key_press(self, keyp, mod):
         if keyp in (key.SPACE):
-            sprite = cocos.sprite.Sprite('start.png')
+            self.set_speed(self._speed + 10)
 
 
 def main():
     cocos.director.director.init(caption="Coolest project ever")
-    entManage = EntityManager(2, 10, "boo")
-    scene = Scene(UI.Layer(entManage))
 
+    entManage = EntityManager(2, 10, "boo")
+    layer = UI.Layer(entManage)
+    entManage.set_layer_obj(layer)
+
+    scene = Scene(layer)
     cocos.director.director.run(scene)
 
 

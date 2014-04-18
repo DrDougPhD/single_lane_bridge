51f042193dcdb73cb5abcefc1345afa965413fd9
madamelic <madeline.a.cameron@gmail.com>
2014-04-17 21:12:39 -0500

    This version works now too
    Added car.png
    Added sprite spawning
    Added first bits of keyboard control
    Added more Entity functions
    Filled in more functions

--- UI.py
+++ UI.py
@@ -1,14 +1,5 @@
-#
-# cocos2d
-# http://cocos2d.org
-#
-# This code is so you can run the samples without installing the package
 import sys
 import os
-import random
-ri = random.randint
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-#
 
 import cocos
 from cocos.director import director
@@ -16,15 +7,23 @@ from cocos.menu import MenuItem
 from cocos.draw import Line
 from cocos.scene import Scene
 import pyglet
+from pyglet.window import key
+import threading
 
 class UI:
     class Canvas(cocos.draw.Canvas):
         def render(self):
             x, y = director.get_window_size()
 
-            build_road_map_thingie = [ [[100, 400], [100, 100]], [[100, 400], [200, 250]], [[200, 250], [400, 250]],
-                                       [[400, 250], [500, 400]], [[500, 400], [500, 100]], [[500, 100], [400, 250]],
-                                       [[100, 100], [200, 250]] ]
+            build_road_map_thingie = [
+              [[100, 400], [100, 100]],
+              [[100, 400], [200, 250]],
+              [[200, 250], [400, 250]],
+              [[400, 250], [500, 400]], 
+              [[500, 400], [500, 100]], 
+              [[500, 100], [400, 250]],
+              [[100, 100], [200, 250]]
+            ]
 
             for (startCoord, endCoord) in build_road_map_thingie:
                 start = startCoord[0], startCoord[1]
@@ -36,44 +35,94 @@ class UI:
                 self.move_to(start)
                 self.line_to(end)
 
+
     class Layer(cocos.layer.Layer):
-        def __init__(self):
+        def __init__(self, entityManager):
             super(UI.Layer, self).__init__()
 
             self.add(UI.Canvas())
             self.schedule(lambda x: 0)
+			
+            entityList = entityManager.GetEntityList()
+
+            for entity in entityList:
+                self.add(entity._sprite)
+
 class Entity():
     import threading
-    def __init__(self, name, speed, direction):
+    running = True
+    pause = False
+    def __init__(self, id, name, speed, direction, pos):
+        self._id = id
         self._name = name
         self._speed = speed
         self._direction = direction
+        self._pos = pos
+        self._status = "Moving"
+        self._sprite = cocos.sprite.Sprite('car.png', scale=0.10)
+        self._sprite.position = 100, 100
 
+    def GetID(self):
+		return self._id
+		
     def Start(self):
-
+        pass
+		
+    def Start(self):
+        pass
+		
     def Stop(self):
+        pass
+		
+    def GetStatus(self):
+        return self._status
 
+    def SetSpeed(self, speed):
+        self._speed = speed
+		
+    def RequestCS(self):
+        pass
+		
     def GetLocation(self):
+        pass
 
 class EntityManager():
+    is_event_handler = True
+
     def __init__(self, entityNum, speed, directions):
         self._speed = speed
+        self._entityList = [ ]
 
         for i in range(entityNum):
-            entity = Entity(i, speed, directions[i])
+            entity = Entity(i, "belh", speed, directions[i], (0, 0))
             self._entityList.append(entity)
 
     def Start(self):
+        for entity in self._entityList:
+            entity.Start()
 
     def Stop(self):
+        for entity in self._entityList:
+            entity.Stop()
+
+    def GetEntityList(self):
+        return self._entityList
+
+    def on_key_press(self, keyp, mod):
+        if keyp in (key.SPACE):
+            sprite = cocos.sprite.Sprite('start.png')
+
+    def Stop(self):
+        pass
 
 
 def main():
     cocos.director.director.init(caption = "Coolest project ever")
-
+    entManage = EntityManager(2, 10, "boo")
+    scene = Scene(UI.Layer(entManage))
     scene = Scene(UI.Layer())
-
     cocos.director.director.run(scene)
+
 
 if __name__ == "__main__":
     main()

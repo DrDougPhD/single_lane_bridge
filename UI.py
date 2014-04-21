cae51e72d02802418fa5a14a3fa1a580ab621df2
madamelic <madeline.a.cameron@gmail.com>
2014-04-20 19:55:26 -0500

    Did a bunch of stuff
    Added Entity.move()
    Added Entity.give_bridge_ticket()
    Added Entity.get_timestamp()
    Added Entity.check_for_bridge()
    Added Entity._timestamp
    Added Entity._road_map
    Added random road selection in Entity.__init__()
    Added Car_Status class
    Updated Entity._status to use Car_Status class
    Added debug print statements
    Broke it

--- UI.py
+++ UI.py
@@ -6,37 +6,45 @@
 #
 #Info:
 #   Speed is Units per Tick
-#   Speed limit is 250 upt
+#   Speed limit is 200 upt
 
 import sys
 import os
 
-import cocos
+import cocos, math, time
 from cocos.director import director
 from cocos.menu import MenuItem
 from cocos.draw import Line
 from cocos.scene import Scene
+from cocos.actions import MoveTo, Rotate
 import pyglet
 from pyglet.window import key
 import threading
 
 
+class Car_Status(): #You can totally tell I am a C# developer... can't you?
+    Moving = 0
+    Waiting = 1
+    Warning = 2
+    On_Bridge = 3
+
 class UI:
     class Canvas(cocos.draw.Canvas):
         def render(self):
+            print "Creating canvas..."
             x, y = director.get_window_size()
 
-            build_road_map_thingie = [
-                [[100, 400], [100, 100]],
+            roadmap = [
+                [[100, 100], [200, 250]],
+                [[100, 100], [100, 400]],
                 [[100, 400], [200, 250]],
                 [[200, 250], [400, 250]],
                 [[400, 250], [500, 400]],
                 [[500, 400], [500, 100]],
                 [[500, 100], [400, 250]],
-                [[100, 100], [200, 250]]
             ]
 
-            for (startCoord, endCoord) in build_road_map_thingie:
+            for (startCoord, endCoord) in roadmap:
                 start = startCoord[0], startCoord[1]
                 end = endCoord[0], endCoord[1]
                 color = 255, 255, 255, 255
@@ -45,43 +53,70 @@ class UI:
                 self.set_stroke_width(width)
                 self.move_to(start)
                 self.line_to(end)
-
+            print "Road map drawn!"
 
     class Layer(cocos.layer.Layer):
         def __init__(self, entityManager):
             super(UI.Layer, self).__init__()
 
+            print "Creating layer..."
+
             self.add(UI.Canvas())
             self.schedule(lambda x: 0)
+            print "Canvas added!"
 
+            print "Adding entities..."
             entityList = entityManager.get_entity_list()
-
             for entity in entityList:
                 self.add(entity.get_sprite())
+                print "Entity " + entity.get_name() + " added!"
 
+            print "Creating assorted UI elements..."
             speedLabel = cocos.text.Label("Speed: ", position=(545, 450),
                                           color=(200, 200, 200, 200))
             self._speedText = cocos.text.Label(str(entityManager.get_speed()), position=(600, 450),
                                                color=(200, 200, 200, 200))
+            print "Adding assorted UI elements..."
             self.add(speedLabel)
             self.add(self._speedText)
 
+            print "Layer created!"
+
         def redraw_speed(self, speed):
+            print "Redrawing speed..."
             self._speedText.element.text = str(speed)
 
 
 class Entity():
     def __init__(self, name, speed, pos, direction):
+        print "Initializing entity " + name + "..."
+        import random
+        random_road = 3
+        while random_road != 3: #"Bridge" is road 3 and no one can start there
+            random_road = random.randrange(0, 6)
+        del random
+        self._current_road = random_road
+        self._timestamp = -1
         self._name = name
         self._speed = speed
         self._direction = direction
-        self._status = "Moving"
+        self._status = Car_Status.Moving
         self._sprite = cocos.sprite.Sprite('car.png', scale=0.10)
-        self._sprite.position = pos
+        self._position = pos
         self._running = False
+        self._road_map = [
+                [[100, 100], [200, 250]],
+                [[100, 100], [100, 400]],
+                [[100, 400], [200, 250]],
+                [[200, 250], [400, 250]],
+                [[400, 250], [500, 400]],
+                [[500, 400], [500, 100]],
+                [[500, 100], [400, 250]],
+            ]
+        print "Entity " + name + " initialized!"
 
-    def get_id(self):
-        return self._id
+    def get_name(self):
+        return self._name
 
     def get_status(self):
         return self._status
@@ -89,19 +124,63 @@ class Entity():
     def set_speed(self, speed):
         self._speed = speed
 
-    def request_cs(self):
-        pass
+    def handle_cs(self):
+        self._timestamp = time.time() #Make a timestamp of "ticket request"
+        running = False
+
+        return False
 
     def get_location(self):
-        pass
+        return self._sprite.position
 
     def get_sprite(self):
         return self._sprite
 
+    def get_timestamp(self):
+        return self._timestamp
+
+    def give_bridge_ticket(self):
+        self._has_ticket = True #Can go over the bridge
+        pass
+
+    def move(self):
+        #if self._direction == "left":
+            #upcomingRoad = (self._current_road + 1) % 3 #(Number of roads - 1) / 2
+        #if self._direction == "right":
+            #upcomingRoad = (self._current_road - 1) % 3 #(Number of roads - 1) / 2
+
+        if self._position > 1 and self._status != Car_Status.Warning: #If the road has been traveled and not on road 2 or 6
+            self._current_road += 1 #Go to the next road
+            self.position = 0 #Start over
+
+        current_road_start_loc = self._road_map[self._current_road][0]
+        current_road_end_loc = self._road_map[self._current_road][1]
+        loc = self.get_location()
+
+        road_length = math.sqrt(math.pow((current_road_end_loc[0] - current_road_start_loc[0]), 2) +
+                               math.pow((current_road_end_loc[1] - current_road_start_loc[1]), 2))
+
+        self._sprite.do(
+            MoveTo(
+                    (
+                        (loc[0] + ((current_road_end_loc[0] - current_road_start_loc[0]) * (self._position[0] * self._speed))),
+                        (loc[1] + ((current_road_end_loc[1] - current_road_start_loc[1]) * (self._position[1] * self._speed)))
+                    ), 1)
+        )
+
+    def check_for_bridge(self):
+        if self._status == Car_Status.Waiting:
+            return self.handle_cs()
+        if ((self._current_road == 2) or (self._current_road == 6) and self._status != Car_Status.Warning):
+            self._status = Car_Status.Warning
+            return True
+
     def start(self):
-        #        running = True
-        #        while running:
-        #           self._sprite.do(MoveTo())
+        #running = True
+        print "Entity " + self._name + " starting..."
+        while self._running:
+            self.move()
+            self.check_for_bridge()
         pass
 
     def stop(self):
@@ -122,6 +201,7 @@ class EntityManager():
             entityThread = threading.Thread(None, entity.start(), "Entity" + str(i))
             self._entityList.append(entity)
             self._threadList.append(entityThread)
+            print "Entity " + str(i) + " added!"
 
     def get_entity_list(self):
         return self._entityList
@@ -152,18 +232,28 @@ class EntityManager():
 
     def on_key_press(self, keyp, mod):
         if keyp in (key.SPACE):
-            self.set_speed(self._speed + 10)
+            speed_increase = self._speed + 10
+            print "Increasing speed to " + str(speed_increase) + "..."
+            self.set_speed(speed_increase)
+        if keyp in (key.ENTER):
+            print "Starting simulation..."
+            self.start()
 
 
 def main():
-    cocos.director.director.init(caption="Coolest project ever")
+    cocos.director.director.init(caption="CS 384 Project")
 
-    entManage = EntityManager(2, 10, "boo")
+    directions = [ "left", "right" ]
+    entManage = EntityManager(2, 10, directions)
     layer = UI.Layer(entManage)
+    print "Setting layer object..."
     entManage.set_layer_obj(layer)
 
+    print "Starting scene..."
     scene = Scene(layer)
+    print "Running scene..."
     cocos.director.director.run(scene)
+    print "Scene running!"
 
 
 if __name__ == "__main__":

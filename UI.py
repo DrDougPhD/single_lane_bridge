77bde770bdbfedd1def911dbe2e956657cc5278a
madamelic <madeline.a.cameron@gmail.com>
2014-04-25 20:00:50 -0500

    Hand-merged code
    Merged a bunch of stuff from master.

--- UI.py
+++ UI.py
@@ -10,34 +10,43 @@
 
 
 import cocos
-from EntityManager import EntityManager
-from EntityManager import Bridge_Mode
+from VehicleManager import Vehicle
+from VehicleManager import Bridge_Mode
 from cocos.director import director
 from cocos.draw import Line
 from cocos.scene import Scene
 from pyglet.window import key
+
+
+class RoadPoints:
+    NW = 100, 100
+    SW = 100, 400
+    NE = 500, 100
+    SE = 500, 400
+
+    # Intersections of the road with the bridge.
+    WEST_BRIDGE_ENTRY = 200, 250
+    EAST_BRIDGE_ENTRY = 400, 250
+
+    ROADMAP = [
+        [NW, WEST_BRIDGE_ENTRY],
+        [NW, SW],
+        [SW, WEST_BRIDGE_ENTRY],
+        [WEST_BRIDGE_ENTRY, EAST_BRIDGE_ENTRY],
+        [EAST_BRIDGE_ENTRY, SE],
+        [SE, NE],
+        [NE, EAST_BRIDGE_ENTRY],
+    ]
 
 class UI:
     class Canvas(cocos.draw.Canvas):
         def render(self):
             print("Creating canvas...")
             x, y = director.get_window_size()
+            color = 255, 255, 255, 255
+            width = 3
 
-            roadmap = [
-                [[100, 100], [200, 250]],
-                [[100, 100], [100, 400]],
-                [[100, 400], [200, 250]],
-                [[200, 250], [400, 250]],
-                [[400, 250], [500, 400]],
-                [[500, 400], [500, 100]],
-                [[500, 100], [400, 250]],
-            ]
-
-            for (startCoord, endCoord) in roadmap:
-                start = startCoord[0], startCoord[1]
-                end = endCoord[0], endCoord[1]
-                color = 255, 255, 255, 255
-                width = 3
+            for start, end in RoadPoints.ROADMAP:
                 self.set_color(color)
                 self.set_stroke_width(width)
                 self.move_to(start)
@@ -91,11 +100,13 @@ class UI:
             if keyp == key.ENTER:
                 print("Starting simulation...")
                 self._entManager.start()
+
+
 def main():
     cocos.director.director.init(caption="CS 384 Project")
 
-    directions = [ "left", "right" ]
-    entManage = EntityManager(2, 10, directions, Bridge_Mode.One_at_a_Time)
+    directions = ["left", "right"]
+    entManage = Vehicle(2, 10, directions, Bridge_Mode.One_at_a_Time)
     layer = UI.Layer(entManage)
     print("Setting layer object...")
     entManage.set_layer_obj(layer)
@@ -107,7 +118,11 @@ def main():
     print("Running scene...")
     cocos.director.director.run(scene)
     print("Scene running!")
+    print("Goodbye!")
 
 
 if __name__ == "__main__":
+    # If you run this python script from the command line, then this
+    #  if-statement will evaluate to true, in which the main() function
+    #  will be executed.
     main()
--- VehicleManager.py
+++ VehicleManager.py
@@ -8,20 +8,20 @@
 #   Speed is Units per Tick
 #   Speed limit is 200 upt
 
-from Entity import Entity
+from Vehicle import Entity
 import random, sys, threading, operator, time
 
 class Bridge_Mode():
     One_at_a_Time = 0
     One_direction = 1
 
-class EntityManager():
+class VehicleManager():
     def __init__(self, entityNum, speed, directions, mode):
-        self._speed = speed
-        self._entityList = []
-        self._threadList = []
-        self._layer = None
-        self._bridge_mode = mode
+        self.speed = speed
+        self.vehicleList = []
+        self.threadList = []
+        self.layer = None
+        self.bridge_mode = mode
         self._running = False
 
         carNameList = [ "Scion", "Toyota", "Ford", "Lamborgini", "Tesla", "Mercedes", "Mazda", "Chevrolet", "BMW",
@@ -29,41 +29,28 @@ class EntityManager():
         for i in range(entityNum):
             randomCar = random.randint(len(carNameList), sys.maxint) % len(carNameList)
             entity = Entity(carNameList[randomCar], speed, directions[i])
-            self._entityList.append(entity)
-            print("Entity " + str(i) + " added!")
-
-    def get_entity_list(self):
-        return self._entityList
-
-    def get_speed(self):
-        return self._speed
-
-    def set_layer_obj(self, layer):
-        self._layer = layer
-        print("Layer object set.")
+            self.entityList.append(entity)
+            print("Vehicle " + str(i) + " added!")
 
     def set_speed(self, speed):
         self._speed = speed
         self._layer.redraw_speed(speed)
 
-        for entity in self._entityList:
-            entity.set_speed(speed)
-
-    def set_bridge_mode(self, mode):
-        self._bridge_mode = mode
+        for vehicle in self.vehicleList:
+            vehicle.set_speed(speed)
 
     def start(self):
         self._running = True
 
-        for entity in self._entityList:
-            entity.start()
+        for vehicle in self.vehicleList:
+            vehicle.start()
 
         timeStampList = []
         while self._running:
-            print("Runn")
-            for entity in self._entityList:
-                if entity.get_status() == "Waiting":
-                    timeStampList.append([entity, entity.get_timestamp()])
+            print("Run")
+            for vehicle in self.vehicleList:
+                if vehicle.status == "Waiting":
+                    timeStampList.append([vehicle, vehicle.timestamp])
 
             if len(timeStampList) > 0:
                 timeStampList.sort(key=operator.itemgetter(2)) #Sort by timestamp / 2nd column
@@ -73,7 +60,7 @@ class EntityManager():
     def stop(self):  #This may not even be needed...
         for thread in self._threadList:
             index = self._threadList.index(thread)
-            entityObj = self._entityList[index]
+            entityObj = self.vehicleList[index]
             entityObj.stop()
             thread.join()
 

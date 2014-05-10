f9f79b24254beac514570c12f8e54d135a33a7a5
Doug McGeehan <djmvfb@mst.edu>
2014-05-10 15:46:57 -0500

    Imports are made one per line as recommended by Python best practices String formatting has replaced string concatenation Vehicle.position is renamed Vehicle.road_completed_percentage Vehicle rotation is delegated to its own function to reduce duplicated code Movement points renamed to shorter names

--- Vehicle.py
+++ Vehicle.py
@@ -4,8 +4,13 @@
 #CS 384 - Distributed Operating Systems
 #Spring 2014
 
-import random, cocos, time, math, threading
-from cocos.actions import MoveTo, RotateTo
+import random
+import cocos
+import time
+import math
+import threading
+from cocos.actions import MoveTo
+from cocos.actions import RotateTo
 from UI import RoadPoints
 
 
@@ -15,13 +20,12 @@ class Car_Status(): #You can totally tell I am a C# developer... can't you?
     Warning = 2
     On_Bridge = 3
 
-class Vehicle():
+class Vehicle:
     def __init__(self, index, speed, direction):
-        print("Initializing vehicle " + str(index) + "...")
+        print("Initializing vehicle {0}...".format(index))
 
-        random_road = 3
-        while random_road == 3: #"Bridge" is road 3 and no one can start there
-            random_road = random.randrange(0, 7)
+        #"Bridge" is road 3 and no one can start there
+        random_road = random.choice([0, 1, 2, 4, 5, 6]) 
 
         self.current_road = random_road
         self.timestamp = -1
@@ -29,9 +33,10 @@ class Vehicle():
         self.speed = speed
         self.direction = direction
         self.status = Car_Status.Moving
-        self.position = 0
+        self.road_completed_percentage = 0
         self.road_map = RoadPoints.ROADMAP
         self.label = None
+
         self.sprite = cocos.sprite.Sprite(
           'car2.png',
           scale=0.10,
@@ -39,6 +44,11 @@ class Vehicle():
         ) #Pick a random color
         self.sprite.position = self.road_map[self.current_road][0]
 
+        self.rotate_vehicle()
+        print("Vehicle {0} initialized!".format(index))
+
+
+    def rotate_vehicle(self):
         if self.current_road % 2 == 0: #Vehicle rotation
             mod = 0
             if self.current_road == 0 or self.current_road == 4:
@@ -62,69 +72,58 @@ class Vehicle():
         if self.current_road == 3:
             self.sprite.do(RotateTo(0, 0))
 
-        print("Vehicle " + str(index) + " initialized!")
 
     def create_timestamp(self):
         self.timestamp = time.time()
 
     def move(self):
-        if self.position >= 1 and self.status != Car_Status.Warning: #If the road has been traveled and not on road 2 or 6
-            self.current_road += 1 #Go to the next road
-            self.position = 0 #Start over
+        # If the road has been traveled and not on road 2 or 6
+        if self.road_completed_percentage >= 1 and self.status != Car_Status.Warning:
+            self.current_road += 1 # Go to the next road
+            self.road_completed_percentage = 0 # Start over
+            self.rotate_vehicle()
 
-            if self.current_road % 2 == 0: #Vehicle rotation
-                mod = 0
-                if self.current_road == 0 or self.current_road == 4:
-                    if self.current_road == 4:
-                        mod = -1
-                    else:
-                        mod = -4.2142857
-                else:
-                    if self.current_road == 2:
-                        mod = 1
-                    else:
-                        mod = 4.2142857
-                self.sprite.do(RotateTo(mod*56, 0))
-            if self.current_road == 1 or self.current_road == 5:
-                mod = 0
-                if self.current_road == 1:
-                    mod = -1
-                else:
-                    mod = 1
-                self.sprite.do(RotateTo(mod*90, 0))
         else:
-            if self.position >= 0.85 and self.status == Car_Status.Warning: #Stop short before bridge, mostly cosmetic
+            # Stop short before bridge, mostly cosmetic
+            if self.road_completed_percentage >= 0.85 and self.status == Car_Status.Warning:
                 self.status = Car_Status.Waiting
                 self.create_timestamp()
                 return
             if self.status == Car_Status.On_Bridge:
-                self.position = 0
+                self.road_completed_percentage = 0
                 self.current_road = 3
                 self.sprite.do(RotateTo(0, 0))
 
-        current_road_start_loc = self.road_map[self.current_road][0]
-        current_road_end_loc = self.road_map[self.current_road][1]
-        road_length = math.sqrt(math.pow((current_road_end_loc[0] - current_road_start_loc[0]), 2) +
-                               math.pow((current_road_end_loc[1] - current_road_start_loc[1]), 2))
+        start, end = self.road_map[self.current_road]
+        start_x, start_y = start
+        end_x, end_y = end
+        road_length = math.hypot(end_x - start_x, end_y - start_y)
 
-        self.position += (1.0 / road_length) * (1 + self.speed / 100) #Step size
-        self.sprite.do(
-            MoveTo(
-                    (
-                        (current_road_start_loc[0] + ((current_road_end_loc[0] - current_road_start_loc[0]) * self.position)),
-                        (current_road_start_loc[1] + ((current_road_end_loc[1] - current_road_start_loc[1]) * self.position))
-                    ), 0.6)
+        self.road_completed_percentage += (1.0 / road_length) * (1 + self.speed / 100) #Step size
+        move_to = (
+          (start_x + ((end_x - start_x) * self.road_completed_percentage)),
+          (start_y + ((end_y - start_y) * self.road_completed_percentage))
         )
+        self.sprite.do(MoveTo(move_to, 0.6))
 
-        self.label.position = (self.sprite.position[0] + 10, self.sprite.position[1] + 10)
+        self.label.position = (
+          move_to[0] + 10,
+          move_to[1] + 10
+        )
+        #label_move_to = sprite_move_to[0], sprite_move_to[0]+10
+        #self.label.do(MoveTo(label_move_to, 0.6))
+
 
     def check_for_bridge(self):
         if self.status == Car_Status.Waiting:
-            print("Vehicle " + str(self.index) + " is waiting at the bridge!")
+            print("Vehicle {0} is waiting at the bridge!".format(self.index))
             #return self.handle_cs()
 
-        if ((self.current_road == 2) or (self.current_road == 6) and
-            (self.status != Car_Status.Warning and self.status != Car_Status.Waiting)):
-            print("Vehicle " + str(self.index) + " is near the bridge!")
+        car_is_approaching_bridge = (
+          (self.current_road == 2) or
+          (self.current_road == 6)
+        )
+        if car_is_approaching_bridge and self.status == Car_Status.Moving:
+            print("Vehicle {0} is near the bridge!".format(self.index))
             self.status = Car_Status.Warning
             return True

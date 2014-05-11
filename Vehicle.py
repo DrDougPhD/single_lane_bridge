581685e1896a663162af2672dd199b2385ec093c
Doug McGeehan <djmvfb@mst.edu>
2014-05-10 21:11:11 -0500

    No livelocks

--- Vehicle.py
+++ Vehicle.py
@@ -4,14 +4,115 @@
 #CS 384 - Distributed Operating Systems
 #Spring 2014
 
-import random
-import cocos
-import time
-import math
-import threading
-from cocos.actions import MoveTo
-from cocos.actions import RotateTo
+import random, cocos, time, math, threading
+from cocos.actions import MoveTo, RotateTo
 from UI import RoadPoints
+from cocos.actions import CallFunc
+import math
+
+def duration(src, dst, speed):
+  x1, y1 = src
+  x2, y2 = dst
+  d = math.hypot(x2 - x1, y2 - y1)
+  return d/float(speed)
+
+
+# Precondition: The process which called this is waiting for the token to
+#  enter the critical section.
+def get_movement_path(starting_point, vehicle):
+  """When a process is given the token for entry on the bridge, it will
+  be permitted to cross the bridge and drive through the town."""
+
+  if starting_point == RoadPoints.W:
+    # Cross the bridge from West to East.
+    #  It is assumed the car is currently at RoadPoints.W
+    #  W -> E -> NE -> SE -> E
+    return (
+      MoveTo(RoadPoints.E, duration(starting_point, RoadPoints.E, vehicle.speed)) + \
+      #Rotate() + \
+      CallFunc(vehicle.leave_bridge) + \
+      get_town_travel_path(RoadPoints.E, vehicle.speed)
+    )
+
+  elif starting_point == RoadPoints.E:
+    # Cross the bridge from East to West.
+    #  It is assumed the car is currently at RoadPoints.W
+    #  E -> W -> SW -> NW -> W
+    return (
+      MoveTo(RoadPoints.W, duration(starting_point, RoadPoints.W, vehicle.speed)) + \
+      #Rotate() + \
+      CallFunc(vehicle.leave_bridge) + \
+      get_town_travel_path(RoadPoints.W, vehicle.speed)
+    )
+
+
+def get_town_travel_path(starting_point, speed):
+  """When a process is given the token for entry on the bridge, it will
+  be permitted to cross the bridge and drive through the town."""
+
+  if starting_point == RoadPoints.E:
+    # Travel the Eastern Town.
+    #  It is assumed the car is currently at RoadPoints.E
+    #  E -> NE -> SE -> E
+    return (
+      #Rotate() + \
+      MoveTo(RoadPoints.NE, duration(RoadPoints.E, RoadPoints.NE, speed)) + \
+      #Rotate() + \
+      MoveTo(RoadPoints.SE, duration(RoadPoints.NE, RoadPoints.SE, speed)) + \
+      #Rotate() + \
+      MoveTo(RoadPoints.E, duration(RoadPoints.SE, RoadPoints.E, speed))
+    )
+
+  elif starting_point == RoadPoints.NE:
+    # Travel the Eastern Town.
+    #  It is assumed the car is currently at RoadPoints.E
+    #  NE -> SE -> E
+    return (
+      #Rotate() + \
+      MoveTo(RoadPoints.SE, duration(RoadPoints.NE, RoadPoints.SE, speed)) + \
+      #Rotate() + \
+      MoveTo(RoadPoints.E, duration(RoadPoints.SE, RoadPoints.E, speed))
+    )
+
+  elif starting_point == RoadPoints.SE:
+    # Travel the Eastern Town.
+    #  It is assumed the car is currently at RoadPoints.E
+    #  SE -> E
+    return (
+      #Rotate() + \
+      MoveTo(RoadPoints.E, duration(RoadPoints.SE, RoadPoints.E, speed))
+    )
+
+  elif starting_point == RoadPoints.W:
+    # Travel the Western Town.
+    #  It is assumed the car is currently at RoadPoints.W
+    #  W -> SW -> NW -> W
+    return (
+      #Rotate() + \
+      MoveTo(RoadPoints.SW, duration(RoadPoints.W, RoadPoints.SW, speed)) + \
+      #Rotate() + \
+      MoveTo(RoadPoints.NW, duration(RoadPoints.SW, RoadPoints.NW, speed)) + \
+      #Rotate() + \
+      MoveTo(RoadPoints.W, duration(RoadPoints.NW, RoadPoints.W, speed))
+    )
+
+  elif starting_point == RoadPoints.SW:
+    # Travel the Western Town.
+    #  SW -> NW -> W
+    return (
+      #Rotate() + \
+      MoveTo(RoadPoints.NW, duration(RoadPoints.SW, RoadPoints.NW, speed)) + \
+      #Rotate() + \
+      MoveTo(RoadPoints.W, duration(RoadPoints.NW, RoadPoints.W, speed))
+    )
+
+  elif starting_point == RoadPoints.NW:
+    # Travel the Western Town.
+    #  NW -> W
+    return (
+      #Rotate() + \
+      MoveTo(RoadPoints.W, duration(RoadPoints.NW, RoadPoints.W, speed))
+    )
 
 
 class Car_Status(): #You can totally tell I am a C# developer... can't you?
@@ -20,110 +121,140 @@ class Car_Status(): #You can totally tell I am a C# developer... can't you?
     Warning = 2
     On_Bridge = 3
 
-class Vehicle:
+class Vehicle():
     def __init__(self, index, speed, direction):
-        print("Initializing vehicle {0}...".format(index))
-
-        #"Bridge" is road 3 and no one can start there
-        random_road = random.choice([0, 1, 2, 4, 5, 6]) 
+        print("Initializing vehicle " + str(index) + "...")
+        
+        # "Bridge" is road 3 and no one can start there
+        random_road = random.choice([0, 1, 2, 4, 5, 6])
+        self.buffered_requests = []
+        self.is_on_bridge = False
+        self.acknowledgements = {}
+        self.other_vehicles = []
+        self.timestamp = None
 
         self.current_road = random_road
-        self.timestamp = -1
         self.index = index
-        self.speed = speed
+        self.speed = random.choice([100, 200, 150, 60, 50]) #speed
         self.direction = direction
         self.status = Car_Status.Moving
-        self.road_completed_percentage = 0
+        self.position = 0
         self.road_map = RoadPoints.ROADMAP
         self.label = None
-
         self.sprite = cocos.sprite.Sprite(
           'car2.png',
           scale=0.10,
           color=[random.randrange(0, 255) for i in range(3)]
         ) #Pick a random color
         self.sprite.position = self.road_map[self.current_road][0]
-
-        self.rotate_vehicle()
-        print("Vehicle {0} initialized!".format(index))
+        print("Vehicle " + str(index) + " initialized!")
 
 
-    def rotate_vehicle(self):
-        if self.current_road % 2 == 0: #Vehicle rotation
-            mod = 0
-            if self.current_road == 0 or self.current_road == 4:
-                if self.current_road == 4:
-                    mod = -1
-                else:
-                    mod = -4.2142857
-            else:
-                if self.current_road == 2:
-                    mod = 1
-                else:
-                    mod = 4.2142857
-            self.sprite.do(RotateTo(mod*56, 0))
-        if self.current_road == 1 or self.current_road == 5:
-            mod = 0
-            if self.current_road == 1:
-                mod = -1
-            else:
-                mod = 1
-            self.sprite.do(RotateTo(mod*90, 0))
-        if self.current_road == 3:
-            self.sprite.do(RotateTo(0, 0))
+    def set_other_vehicles(self, other_vehicles):
+      # By referencing the original list, any modification to it will
+      #  immediately be reflected in the local list. That's just how
+      #  Python lists work.
+      self.other_vehicles = other_vehicles
 
 
-    def create_timestamp(self):
-        self.timestamp = time.time()
+    def begin(self):
+      print(self.other_vehicles)
+      drive_path = get_town_travel_path(self.road_map[self.current_road][0], self.speed)
+      request_bridge_access = CallFunc(self.request_access_to_bridge)
+      self.sprite.do(drive_path + request_bridge_access) 
 
-    def move(self):
-        # If the road has been traveled and not on road 2 or 6
-        if self.road_completed_percentage >= 1 and self.status != Car_Status.Warning:
-            self.current_road += 1 # Go to the next road
-            self.road_completed_percentage = 0 # Start over
-            self.rotate_vehicle()
+
+    def __repr__(self):
+      return str(self)
+
+
+    def __str__(self):
+      return "{0}".format(self.index)
+
+
+    def request_access_to_bridge(self):
+      # Record the time and reset the acknowledgement tracker.
+      now = time.time()
+      self.timestamp = now
+      print("{0} is requesting bridge access".format(self.index))
+      self.acknowledgements = {v:False for v in self.other_vehicles if v.index != self.index}
+
+      # As per Ricart & Agrawala's algorithm, broadcast a timestamped request
+      # to all other vehicles.
+      for c in self.other_vehicles:
+        if (c.index != self.index):
+          print("{0} sent request to {1}".format(self.index, c.index))
+          c.request(self, now)
+
+
+    def request(self, requester, t):
+      # As per Ricart & Agrawala's algorithm, the requester has broadcast a
+      #  timestamped request to all vehicles. This vehicle has received the
+      #  request and checks if it needs access to the bridge. Return an 
+      #  acknowledgement if:
+      #    1. This vehicle does not need access to the bridge
+      #    2. This vehicle's request occurred later than the other vehicle
+      # If we are already on the bridge, then buffer this request until we
+      #  have exited the bridge.
+      print("{0} received request from {1}".format(self.index, requester.index))
+      
+      if self.is_on_bridge:
+        print("{0} buffering request from {1}".format(self.index, requester.index))
+        self.buffered_requests.append(requester)
+
+      else:
+        # This car is not on the bridge. However, it could still have a
+        #  request that is newer than the received request.
+        timestamp_is_newer = (self.timestamp > t)
+        has_higher_ID_and_timestamps_equal = (
+          self.index > requester.index and
+          self.timestamp == t
+        )
+        # If there is no pending request, or if the pending request is
+        #  newer than the sent one, or if they are equal and the sender
+        #  has a lower ID, then grant them their acknowledgment.
+        if (self.timestamp is None or
+            timestamp_is_newer or
+            has_higher_ID_and_timestamps_equal):
+          print("{0} sends ack to {1}".format(self.index, requester.index))
+          requester.acknowledge(self)
 
         else:
-            # Stop short before bridge, mostly cosmetic
-            if self.road_completed_percentage >= 0.85 and self.status == Car_Status.Warning:
-                self.status = Car_Status.Waiting
-                self.create_timestamp()
-                return
-            if self.status == Car_Status.On_Bridge:
-                self.road_completed_percentage = 0
-                self.current_road = 3
-                self.sprite.do(RotateTo(0, 0))
-
-        start, end = self.road_map[self.current_road]
-        start_x, start_y = start
-        end_x, end_y = end
-        road_length = math.hypot(end_x - start_x, end_y - start_y)
-
-        self.road_completed_percentage += (1.0 / road_length) * (1 + self.speed / 100) #Step size
-        move_to = (
-          (start_x + ((end_x - start_x) * self.road_completed_percentage)),
-          (start_y + ((end_y - start_y) * self.road_completed_percentage))
-        )
-        self.sprite.do(MoveTo(move_to, 0.6))
-
-        self.label.position = (
-          move_to[0] + 10,
-          move_to[1] + 10
-        )
-        #label_move_to = sprite_move_to[0], sprite_move_to[0]+10
-        #self.label.do(MoveTo(label_move_to, 0.6))
+          print("{0} buffering request from {1}".format(self.index, requester.index))
+          self.buffered_requests.append(requester)
 
 
-    def check_for_bridge(self):
-        if self.status == Car_Status.Waiting:
-            print("Vehicle {0} is waiting at the bridge!".format(self.index))
-            #return self.handle_cs()
+    def leave_bridge(self):
+      print("{0} releases token to {1} pending requests".format(
+        self.index,
+        len(self.buffered_requests)
+      ))
+      self.is_on_bridge = False
+      while self.buffered_requests:
+        v = self.buffered_requests.pop(0)
+        v.acknowledge(self)
 
-        car_is_approaching_bridge = (
-          (self.current_road == 2) or
-          (self.current_road == 6)
-        )
-        if car_is_approaching_bridge and self.status == Car_Status.Moving:
-            print("Vehicle {0} is near the bridge!".format(self.index))
-            self.status = Car_Status.Warning
-            return True
+
+    def acknowledge(self, other):
+      # This car is granted an acknowledgement to its request from the
+      #  grantingVehicle
+      print("{0} received ack from {1}".format(self.index, other.index))
+      self.acknowledgements[other] = True
+      for v in self.acknowledgements:
+        print("{0} checking for acknowledgement for {1}: {2}".format(
+          self.index,
+          v.index,
+          self.acknowledgements[v]
+        ))
+
+      if all(self.acknowledgements.values()):
+        print("All acks received! Crossing bridge.")
+        self.cross_bridge()
+
+
+    def cross_bridge(self):
+      self.timestamp = None
+      self.is_on_bridge = True
+      drive_path = get_movement_path(self.sprite.position, self)
+      request_bridge_access = CallFunc(self.request_access_to_bridge)
+      self.sprite.do(drive_path + request_bridge_access)
--- VehicleManager.py
+++ VehicleManager.py
@@ -4,34 +4,35 @@
 #CS 384 - Distributed Operating Systems
 #Spring 2014
 
-from Vehicle import Car_Status
-from Vehicle import Vehicle
+from Vehicle import *
 import random
-import threading
 import sys
+import threading
 import operator
 import time
 
-class Bridge_Mode:
+class Bridge_Mode():
     One_at_a_Time = -1
     One_direction = 1
 
-class VehicleManager(threading.Thread):
+class VehicleManager:#(threading.Thread):
     def __init__(self, numVehicles, speed, directions, mode):
-        threading.Thread.__init__(self)
+        #threading.Thread.__init__(self)
         self.stopEvent = threading.Event()
-
         self.speed = speed
+        self.vehicleList = []
         self.layer = None
         self.bridge_mode = mode
         self.running = False
+        self.tick = 0.5
 
-        self.vehicleList = []
         for i in range(numVehicles):
             vehicle = Vehicle(str(i), speed, directions[i])
             self.vehicleList.append(vehicle)
-            print("Vehicle {0} added!".format(i))
+            print("Vehicle " + str(i) + " added!")
 
+        for i in self.vehicleList:
+            i.set_other_vehicles(self.vehicleList)
 
     def set_speed(self, speed):
         self.speed = speed
@@ -40,13 +41,20 @@ class VehicleManager(threading.Thread):
         for vehicle in self.vehicleList:
             vehicle.set_speed(speed)
 
-
     def add_vehicle(self, direction):
         vehicle = Vehicle(str(len(self.vehicleList)), self.speed, direction)
         self.vehicleList.append(vehicle)
+        vehicle.set_other_vehicles(self.vehicleList)
+
         self.layer.add_vehicle(vehicle)
         self.layer.create_speed_label(vehicle=vehicle)
         self.layer.create_vehicle_label(vehicle=vehicle)
+
+
+    def start(self):
+      print("Starting!!!!!")
+      for v in self.vehicleList:
+        v.begin()
 
 
     def run(self):
@@ -70,13 +78,9 @@ class VehicleManager(threading.Thread):
 
             if len(timeStampList) > 0:
                 #timeStampList.sort(key=operator.itemgetter(2)) #Sort by timestamp / 2nd column
-                print("Oldest timestamp: {0} from vehicle {1}".format(
-                  timeStampList[0][1], 
-                  timeStampList[0][0].index
-                ))
+                print("Oldest timestamp: " + str(timeStampList[0][1]) + " from vehicle " + str(timeStampList[0][0].index))
 
             time.sleep(0.5) #Arbitrary
-
 
     def stop(self):  #This may not even be needed...
         self.stopEvent.set()

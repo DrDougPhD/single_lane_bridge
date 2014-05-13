3ae8986911522f7525320e7d426b6e679019173a
Doug McGeehan <djmvfb@mst.edu>
2014-05-13 12:48:01 -0500

    added comments to the settings.py file

--- settings.py
+++ settings.py
@@ -1,4 +1,10 @@
+# settings.py
+#  Default settings for the number of vehicles and the choice of speeds for
+#  the bridge crossing program.
+#
+# Written by Madeline Cameron and Doug McGeehan
+# CS 384 - Distributed Operating Systems
+# Spring 2014
+
 DEFAULT_SPEED_CHOICES = [50, 100, 150, 250, 400]
 DEFAULT_NUM_VEHICLES = 5
-
-

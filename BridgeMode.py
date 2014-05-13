a11e5aa7161d6bb66700e9fbf915e1dd02c9aa4f
Doug McGeehan <djmvfb@mst.edu>
2014-05-13 12:44:44 -0500

    Added header comment

--- BridgeMode.py
+++ BridgeMode.py
@@ -1,7 +1,16 @@
+# BridgeMode.py
+#
+#  Enumeration class for the two types of bridge-crossing modes that vehicles
+#  may adopt.
+#
+# Written by Madeline Cameron and Doug McGeehan
+# CS 384 - Distributed Operating Systems
+# Spring 2014
+
+
 class BridgeMode():
   One_at_a_Time = 0
   One_direction = 1
-
 
   def __contains__(self, mode):
     # Convenience function to test if the user supplied a valid

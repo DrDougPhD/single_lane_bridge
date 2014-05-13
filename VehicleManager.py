ead2a7b8e9d37c4471cadb64dca53cd2b169f1a2
Doug McGeehan <djmvfb@mst.edu>
2014-05-13 12:28:56 -0500

    With custom speeds, the set_speed() function of the VehicleManager is no longer needed

--- VehicleManager.py
+++ VehicleManager.py
@@ -6,35 +6,22 @@
 
 from Vehicle import getVehicleClassByMode
 class VehicleManager:
-    def __init__(self, numVehicles, speed, mode):
-        self.speed = speed
+    def __init__(self, speeds, mode):
         self.vehicleList = []
         self.layer = None
         self.vehicle_class = getVehicleClassByMode(mode)
-        self.running = False
-        self.tick = 0.5
 
-        for i in range(numVehicles):
-            vehicle = self.vehicle_class(
-              index=i,
-              speed=speed,
-            )
+        for s in speeds:
+            vehicle = self.vehicle_class(speed=s)
             self.vehicleList.append(vehicle)
             print("Vehicle {0} added!".format(vehicle))
 
-        for i in self.vehicleList:
-            i.set_other_vehicles(self.vehicleList)
+        for v in self.vehicleList:
+            v.set_other_vehicles(self.vehicleList)
 
-    def set_speed(self, speed):
-        self.speed = speed
-        self.layer.redraw_speed(speed)
-
-        for vehicle in self.vehicleList:
-            vehicle.set_speed(speed)
-
+    
     def add_vehicle(self):
         vehicle = self.vehicle_class(
-          index=len(self.vehicleList),
           speed=self.speed
         )
         self.vehicleList.append(vehicle)
